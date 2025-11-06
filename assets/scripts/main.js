5fbebabb18399cdbbfc452d18067e7f9a90b86c7
Your Name <you@example.com>
2025-11-06 17:12:23 +0500

    chore: fixed nav links, update header layout, feat: add new responsive pages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -63,6 +63,52 @@ $(function () {
                     },
                 ]
             }
+        } else if (slider.hasClass('serteficates')) {
+            extraOptions = {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                centerMode: false,
+                arrows: true,
+                dots: false,
+                autoplay: true,
+                adaptiveHeight: true,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 2.5,
+                            slidesToScroll: 1,
+                            dots: true,
+                        }
+                    },
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                            dots: true,
+                            adaptiveHeight: true,
+                        }
+                    }, {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1.5,
+                            slidesToScroll: 1,
+                            dots: true,
+                            adaptiveHeight: true,
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            dots: true,
+                            adaptiveHeight: true,
+                        }
+                    },
+                ]
+            }
         }
         slider.slick($.extend({}, extraOptions, options));
     })
